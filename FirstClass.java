bafcf039c74e941b8a1d416fad52ffd108e5eb2f
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2022-10-07 10:22:20 +0530

    Add files via upload

---
+++ FirstClass.java
@@ -0,0 +1,29 @@
+// Java is OOPS 
+// Everything is done by using Object.
+// Every program in Java Start with a Class.
+// Class (PascalCase) contains the Features which required by Object.
+/*
+ * Syntax 
+ * class <ClassName>{
+ * }
+ */
+class SecondClass{ // class start
+ // Entry Gate
+ // main is a function, function contains logic (Statements)
+ // void - keyword (Reserve Word) - No Return 
+// static - keyword - When class is loaded in Memory 
+//, then static will load along 
+// static (Eager Loading)
+// public - Access outside the package(Folder)
+// String args [] - Command Line Arguments
+// String - collection of chars, String is a Predefineclass
+// args - arguments 
+// [] - BOX or Array  (Contain lot of things)
+ public static void main(String []args){ //main start
+        System.out.println("Hello Java");
+        // print is a predefine function , used to print on console.
+        // print function is access using out object
+        // out object belongs to PrintStream class
+        // out object is created by System class.
+ } // main end
+} // class end
