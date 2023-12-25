59e9ef2b47b96fac1825f1dbcbad4fc4af866d79
Kashish tyagi <tyagikashish98@gmail.com>
2023-12-25 21:09:44 +0530

    add  ahtml file

--- One.java
+++ One.java
@@ -1,15 +1,14 @@
 // VSCode 
 // Data Types + Scanner
 // if else , Loops
-class One{
+class One {
     public static void main(String[] args) {
         int day = 3;
-        if(day ==7){
+        if (day == 7) {
             System.out.println("Today is Holiday");
-        }
-        else{
+            System.out.println("enjoy yours day ");
+        } else {
             System.out.println("Working Day");
         }
     }
 }
-
